d7611f7f6105948f22e1f4ae5420dce6cb4bb02d
pavpench <p.penchev90@gmail.com>
2023-08-04 14:31:52 +0200

    removing style to show UI, adding displayMovements fn

--- script.js
+++ script.js
@@ -72,3 +72,20 @@ const currencies = new Map([
 ]);
 
 /////////////////////////////////////////////////
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+
+  movements.forEach((movement, index) => {
+    const type = movement > 0 ? "deposit" : "withdrawal";
+
+    const html = `<div class="movements__row">
+    <div class="movements__type movements__type--${type}">${
+      index + 1
+    } ${type} </div>
+    <div class="movements__value">${movement}</div>
+                  </div>`;
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+displayMovements(account1.movements);
